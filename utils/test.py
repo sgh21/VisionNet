888bcef76b223ff275ab8074fd8da56d9730a61c
sgh_Y9000P_Ubuntu <sgh21@mails.tsinghua.edu.cn>
2025-03-26 19:42:51 +0800

    add cat dim=-1

--- utils/test.py
+++ utils/test.py
@@ -8,3 +8,10 @@ for i in range(total):
     if num < sample_ratio:
         count += 1
 print(f"The sample_ratio is:{sample_ratio*100}%, but the real value is :{count/total*100}%")
+
+
+import torch
+a = torch.randn(2,3)
+b = torch.randn(2,3)
+c = torch.cat((a,b),dim=-1)
+print(c.shape)
